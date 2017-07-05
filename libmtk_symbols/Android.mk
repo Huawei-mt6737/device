5ad6447b7b1af48cdfc3a1b7e1bf2a2a73949ee5
mdeejay <mdjrussia@gmail.com>
2017-07-05 22:28:30 +0700

    add missed shims

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -2,16 +2,23 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
+    mtk_gui.cpp \
+    mtk_ui.cpp \
+    mtk_omx.cpp \
     icu55.c \
     icu53.c \
     ssl.c \
 
 # only for 32bit libraries
 LOCAL_SRC_FILES_32 := mtk_string.cpp
+LOCAL_SRC_FILES_32 += mtk_wvm32.cpp
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
+LOCAL_SRC_FILES_64 += mtk_wvm64.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libicuuc libicui18n libcrypto
+
+LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
+                          libicuuc libicui18n libcrypto libstagefright_foundation
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
---
+++ libmtk_symbols/mtk_wvm64.cpp
@@ -0,0 +1,6 @@
+extern "C" {
+int _ZN7android16MediaBufferGroupC1Em();
+int _ZN7android16MediaBufferGroupC1Ev() {
+    return _ZN7android16MediaBufferGroupC1Em();
+}
+}
